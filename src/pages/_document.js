3677875f1629035a469a2fddce81f795d315100b
DarBaala <joker.milan1997@gmail.com>
2023-04-27 17:39:40 +0300

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,24 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&family=Merienda&family=Shantell+Sans:wght@400;500&display=swap"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
