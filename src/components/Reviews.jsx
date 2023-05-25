de438e88d6f190cd4182d4b0adb3ee74e7537a68
DarBaala <joker.milan1997@gmail.com>
2023-05-25 10:32:48 +0300

    fix

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -71,17 +71,6 @@ export default function Reviews() {
       >
         {imgLink && (
           <div className="reviews__modal-wrapper">
-            {/* <svg
-              onClick={() => setModal(!modal)}
-              viewBox="0 0 32 32"
-              stroke="#852e3b"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="cross">
-                <line class="cls-1" x1="7" x2="25" y1="7" y2="25" />
-                <line class="cls-1" x1="7" x2="25" y1="25" y2="7" />
-              </g>
-            </svg> */}
             <svg onClick={() => setModal(!modal)} viewBox="0 0 512 512">
               <path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z" />
             </svg>
